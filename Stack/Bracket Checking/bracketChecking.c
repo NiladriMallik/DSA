40680d7e17ea6160bef80dee0c8d8c1d521f6f3a
niladri.mallik007@gmail.com <niladri.mallik007@gmail.com>
2023-12-20 22:33:01 +0530

    Extra side quest to check the order of precendence of brackets

--- Stack/Bracket Checking/bracketChecking.c
+++ Stack/Bracket Checking/bracketChecking.c
@@ -8,6 +8,7 @@ typedef struct{
     int top;
 } Stack;
 
+char precendence[6] = {'[' , '{' , '(' , ')' , '}' , ']'};
 //function prototypes
 
 //function for stack operations
@@ -22,6 +23,9 @@ int isOpeningMatch(char,char);
 
 //actual implementation of the algorithm
 int checkExpr(char[]);
+
+//check valid order of use of brackets
+int correctBracketOrder(char, char);
 
 //push operation
 void push(Stack *sp, char v){
@@ -44,16 +48,16 @@ char pop(Stack *sp){
 }
 
 //returns 1 if stack is empty, 0 otherwise
-
 int isEmpty(Stack *sp){
     return sp->top == -1;
 }
 
 char stackTop(Stack *sp){
     if(isEmpty(sp)){
-        return ' ';
+        printf("Stack underflow, terminating program");
+        exit(1);
     }
-    else return sp->item[sp->top];
+    return sp->item[sp->top];
 }
 
 int isOpeningMatch(char left, char right){
@@ -80,37 +84,56 @@ int isOpeningMatch(char left, char right){
 int checkExpr(char expr[]){
 
     int i = 0,error = 0;
-    char c;
-    Stack *sp;
-    sp->top = -1;
+    
+    Stack stack;
+    stack.top = -1;
     while(expr[i] != '\0'){
-        c = expr[i];
-        if (c == '(' || c == '[' || c == '}'){
-            push(&sp,c);
+        //get the next character
+        char c = expr[i];
+        if (c == '(' || c == '[' || c == '{'){
+            push(&stack,c);
+        /*  printf("\nItems in stack: ");
+                for(int j=0;j<=i;j++){
+                    printf("%c ",stack.item[i]);
+            }
+        */ 
         }
-
         else if(c == ')' || c == '}' || c == ']'){
-            if(isEmpty(&sp)){
+            if(isEmpty(&stack)){
                 error = 1;
                 break;
             }
-            else if(isOpeningMatch(stackTop(&sp),c)){
-                pop(&sp);
+            //check if the top of the stack matches with the character
+            else if(isOpeningMatch(stackTop(&stack),c)){
+                pop(&stack);
+            /*  printf("\nItems in stack: ");
+                for(int j=0;j<=i;j++){
+                    printf("%c ",stack.item[i]);
+                }
+            */ 
             }
             else{
                 error = 1;
                 break;
             }
         }
-        else continue;
+        ++i;
     }
-
-    if(!error && !isEmpty(&sp)){
+    //if we do not have any error but stack is still not empty
+    if(!error && !isEmpty(&stack)){
         error = 1;
     }
-
     return error;
+}
 
+int correctBracketOrder(char stackTop, char right){
+
+    //correct order of brackets {[( )]} 
+    int warn = 0;
+
+    
+
+    return warn;
 }
 
 int main(){
