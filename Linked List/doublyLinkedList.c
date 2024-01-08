3450d15730d87d2bee069641ae3a9e870690d8ea
niladri.mallik007@gmail.com <niladri.mallik007@gmail.com>
2024-01-08 22:27:41 +0530

    doubly linked list initial commit 2

--- Linked List/doublyLinkedList.c
+++ Linked List/doublyLinkedList.c
@@ -21,7 +21,7 @@ typedef struct{
 }LinkedList;
 
 void initList(LinkedList *);
-int loadFromFil(LinkedList *, char *);
+int loadFromFile(LinkedList *, char *);
 void createListFromRandomNumbers(LinkedList *, int);
 void insertAtHead(LinkedList *, int);
 void insertAtTail(LinkedList *, int);
@@ -34,3 +34,95 @@ int deleteLast(LinkedList *);
 int deleteTarget(LinkedList *,int);
 
 int reverse(LinkedList *);
+
+void initList(LinkedList *listptr){
+    listptr->head = NULL;
+    listptr->tail = NULL;
+    listptr->nodeCount = 0;
+}
+
+
+void insertAtHead(LinkedList *listptr, int value){
+    Node *node = (Node*)malloc(sizeof(Node));
+    if(node == NULL){
+        printf("Unable to allocate new node.\n");
+        return;
+    }
+    node->data = value;
+    node->next = NULL;
+    node->prev = NULL;
+    if(listptr->nodeCount == 0){
+        listptr->head = node;
+        listptr->tail = node;
+    }
+    else{
+        node->next = listptr->head;
+        listptr->head = node;
+    }
+    listptr->nodeCount++;
+}
+
+
+void insertAtTail(LinkedList *listptr, int value){
+    Node *node = (Node*)malloc(sizeof(Node));
+    if(node == NULL){
+        printf("Unable to allocate new node.\n");
+        return;
+    }
+    node->data = value;
+    node->next = NULL;
+    if(listptr->nodeCount == 0){
+        listptr->head = node;
+        listptr->tail = node;
+    }
+    else{
+        listptr->tail->next = node;
+        node->prev = listptr->tail;
+        listptr->tail = node;
+    }
+    listptr->nodeCount++;
+}
+
+
+int deleteFirst(LinkedList *listptr){
+    int result;
+    if(listptr->nodeCount == 0){
+        return 0;
+    }
+    Node *temp = listptr->head;
+    if(listptr->nodeCount == 1){
+        listptr->head = NULL;
+        listptr->tail = NULL;
+    }
+    else{
+        listptr->head = temp->next;
+        listptr->head->prev = NULL;
+    }
+    listptr->nodeCount--;
+    result = temp->data;
+    free(temp);
+    return result;
+}
+
+
+int deleteLast(LinkedList *listptr){
+    int result;
+    if(listptr->nodeCount == 0){
+        return 0;
+    }
+    Node *temp = listptr->tail;
+    if(listptr->nodeCount == 1){
+        listptr->head = NULL;
+        listptr->tail = NULL;
+    }
+    else{
+        listptr->tail = temp->prev;
+        listptr->tail->next = NULL;
+        temp->prev = NULL;
+    }
+    result = temp->data;
+    free(temp);
+    listptr->nodeCount--;
+    return result;
+
+}
